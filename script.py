497416f83f9efd900ab4e0ff2bc88c9f9750c663
bvbishnoi33-png <bvbishnoi33@gmail.com>
2026-02-27 20:21:01 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,8 @@
+# script.py
+
+def run_tool(phone):
+    # your encrypted code already present
+    # modify only input/output part if needed
+
+    result = f"Details for {phone} found"
+    return result
