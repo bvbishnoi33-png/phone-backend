7698374abb675c3e4dff1ea362baebf4b0475070
bvbishnoi33-png <bvbishnoi33@gmail.com>
2026-02-27 20:21:41 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,37 @@
+# app.py
+
+from flask import Flask, request, jsonify
+from script import run_tool
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    return "Backend is running"
+
+@app.route("/search", methods=["POST"])
+def search():
+    data = request.get_json()
+
+    phone = data.get("number")
+
+    if not phone or not phone.isdigit() or len(phone) != 10:
+        return jsonify({
+            "status": "error",
+            "message": "Invalid phone number"
+        })
+
+    try:
+        result = run_tool(phone)
+        return jsonify({
+            "status": "success",
+            "data": result
+        })
+    except Exception as e:
+        return jsonify({
+            "status": "error",
+            "message": str(e)
+        })
+
+if __name__ == "__main__":
+    app.run()
